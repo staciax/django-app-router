63e8c6e11d636b4fa7159f3fb12d0a2422abfb43
staciax <stacia.dev@gmail.com>
2024-02-24 06:45:44 +0700

    fixes lint

--- example/app_test/templates/info/page.py
+++ example/app_test/templates/info/page.py
@@ -4,4 +4,4 @@ from django.http.response import HttpResponse
 
 def page(request: HttpRequest) -> HttpResponse:
     """info"""
-    return HttpResponse('<h1>info</h1>')
+    return HttpResponse('<h1>info</h1>'.encode())
