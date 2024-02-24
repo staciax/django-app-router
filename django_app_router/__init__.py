ce5a215fef0c908c1bcfb426854a7ac9ae3ec4ec
staciax <stacia.dev@gmail.com>
2024-02-24 14:34:24 +0700

    Version bump for development version

--- django_app_router/__init__.py
+++ django_app_router/__init__.py
@@ -10,7 +10,7 @@ __title__ = 'django_app_router'
 __author__ = 'STACiA'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2024-present STACiA(staciax)'
-__version__ = '0.0.2'
+__version__ = '0.0.3a'
 
 
 from typing import Literal, NamedTuple
@@ -25,6 +25,6 @@ class VersionInfo(NamedTuple):
     release: Literal['alpha', 'beta', 'final']
 
 
-version_info: VersionInfo = VersionInfo(major=0, minor=0, micro=2, release='final')
+version_info: VersionInfo = VersionInfo(major=0, minor=0, micro=3, release='alpha')
 
 del NamedTuple, Literal, VersionInfo
